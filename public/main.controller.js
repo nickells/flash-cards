41155f7374c13cb7aa789ef8e45fbe327690ed6a
nickells <ellsworth.nick@gmail.com>
2015-07-09 17:25:58 -0400

    categories etc

--- public/main.controller.js
+++ public/main.controller.js
@@ -1,5 +1,10 @@
-app.controller('MainController', function ($scope, whateverName) {
-	$scope.flashCards = whateverName;
+app.controller('MainController', function ($scope,FlashCardsFactory) {
+	FlashCardsFactory.getFlashCards().then(function(data,err){
+		$scope.flashCards = data
+		console.log(data)
+	})
+
+	// console.log ($scope.flashCards)
 
 	$scope.answerQuestion = function (answer, flashCard) {
 		if (!flashCard.answered) {
@@ -7,4 +12,32 @@ app.controller('MainController', function ($scope, whateverName) {
 			flashCard.answeredCorrectly = answer.correct;
 		}
 	}
+
+	$scope.categories = [
+	'All Categories',
+	'MongoDB',
+	'Express',
+	'Angular',
+	'Node'
+	]
+
+	$scope.getCategoryCards = function(category){
+			$scope.selectedCategory = category;
+		if(category==='All Categories'){
+			FlashCardsFactory.getFlashCards().then(function(data,err){
+			$scope.flashCards = data
+			console.log(data)
+		})
+		} else {
+			console.log($scope)
+			FlashCardsFactory.getFlashCards(category).then(function(data,err){
+				$scope.flashCards = data
+				console.log(data)
+			})
+			
+		}
+		isActive = true;
+	}
+
+
 });
