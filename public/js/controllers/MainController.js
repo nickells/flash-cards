1c7077a7a425207a79152ba0e15df2db379e7dba
thang239 <thanghnbk@gmail.com>
2015-07-13 11:19:57 -0400

    refactorized some files

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -2,14 +2,8 @@ app.controller('MainController', function ($scope,FlashCardsFactory,ScoreFactory
 	$scope.showLoadingIcon = true;
 	FlashCardsFactory.getFlashCards().then(function(data,err){
 		$scope.flashCards = data;
-	})
-	.then(function(){
 		$scope.showLoadingIcon = false;
 	})
-	// debugger;
-	// console.log($scope.showLoadingIcon)
-	
-
 	$scope.categories = [
 	'All Categories',
 	'MongoDB',
@@ -20,31 +14,19 @@ app.controller('MainController', function ($scope,FlashCardsFactory,ScoreFactory
 
 	$scope.getCategoryCards = function(category){
 			$scope.showLoadingIcon = true;
-			// debugger;
 			console.log($scope.showLoadingIcon)
 			$scope.selectedCategory = category;
-			// $scope.showFlashcard=false;
 		if(category==='All Categories'){
 			FlashCardsFactory.getFlashCards().then(function(data,err){
 				$scope.flashCards = data;
-			// $scope.showFlashcard1=true;
-			// console.log(data)
-			})
-			.then(function(){
 				$scope.showLoadingIcon=false;
 			})
 		} else {
-			// console.log($scope)
 			FlashCardsFactory.getFlashCards(category).then(function(data,err){
 				$scope.flashCards = data;
-				// $scope.showFlashcard1=true;
-			})
-			.then(function(){
 				$scope.showLoadingIcon=false;
-			})
-			
+			})	
 		}
-			// $scope.showFlashcard=false;
 		isActive = true;
 	}
 
